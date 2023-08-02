bd17bac624d6dbb868f9fba1980f1bcccd0c57dc
rahimbtc <codecrafter1994@gmail.com>
2023-08-02 11:18:44 +0100

    Introducing automated testing

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -7,19 +9,32 @@ from .models import Question, Choice
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "index.html", context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {"latest_question_list": latest_question_list}
+#     return render(request, "index.html", context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "results.html"
 
 
 def vote(request, question_id):
