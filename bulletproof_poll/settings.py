c3fef287216ee3629bfd04b510ef5f6741474363
rahimbtc <codecrafter1994@gmail.com>
2023-08-02 13:30:04 +0100

    Installing Django Debug Toolbar

--- bulletproof_poll/settings.py
+++ bulletproof_poll/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "debug_toolbar",
+
     'polls.apps.PollsConfig',
 ]
 
@@ -49,6 +51,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'bulletproof_poll.urls'
--- bulletproof_poll/urls.py
+++ bulletproof_poll/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path("__debug__/", include("debug_toolbar.urls")),
+
     path("polls/", include("polls.urls")),
 ]
